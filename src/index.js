8279367399561e4ae0ab0ef83bb085dd83071b8f
fkelley1 <kelleyf@wit.edu>
2021-11-08 20:00:38 -0500

    adding form to handle input and template for list to get each input

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
-import React from 'react';
+
+import { useState } from "react";
 import ReactDOM from 'react-dom';
 
-const myelement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-);
+function JournalEntryForm() {
+  const [entry, setEntry] = useState("");
+  const journalItems = [
+    {id:1, value:'Entry1'},
+    {id:2, value:'Entry2'}
+  ];
 
-ReactDOM.render(myelement, document.getElementById('root'));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(entry);
+    journalItems.push(entry);
+  }
+
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <label>Enter your Journal Entry:
+        <input 
+          type="text" 
+          value={entry}
+          onChange={(e) => setEntry(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+    <ul>
+      {journalItems.map((journalItem) => <JournalEntry key={journalItem.id} value={journalItem.value} />)}
+    </ul>
+    </>
+
+  )
+}
+
+function JournalEntry(props){
+  return(
+      <li>{props.value}</li>
+  );
+}
+
+
+ReactDOM.render(<JournalEntryForm />, document.getElementById('root'));
